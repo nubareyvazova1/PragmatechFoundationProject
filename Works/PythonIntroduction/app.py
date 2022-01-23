9e32d14bea340db8b6d23e7c6283962f820880ee
Nubar Eyvazova <eyvazovanubat480@gmail.com>
2022-01-23 15:36:49 +0400

    update

---
+++ Works/PythonIntroduction/app.py
@@ -0,0 +1,10 @@
+adeyisen=input('deyisen elave edin:')
+print(type(deyisen))
+if type(deyisen) =int:   
+   print('deyisen ededdir') 
+else:
+   print('eded deyil')  
+#deyisen=str(input())
+#a=list(deyisen) 
+#a.reserve ()  
+#print(a)                                                                                       
---
+++ Works/PythonIntroduction/python.py
@@ -0,0 +1,10 @@
+for x in range(501):
+    if x%10>0  and x%100=0:
+        cem=cem+x
+        x=x+1
+        print('cem')
+for x in range(501):
+    if x%100>=0  :
+        cem=cem+x
+        x=x+1
+        print('cem')        
